7339b26add52504f00eba83af6f3bfbeae32fb65
Michal Gawik <gawlik.zator@o2.pl>
2024-11-16 07:19:37 +0100

    add route to create job

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import morgan from 'morgan'
+import { nanoid } from 'nanoid'
 
 const app = express()
+
+let jobs = [
+	{ id: nanoid(), company: 'Apple', position: 'front-end developer' },
+	{ id: nanoid(), company: 'Google', position: 'back-end developer' },
+]
 
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
@@ -18,6 +24,27 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
 	console.log(req)
 	res.json({ message: 'data received', data: req.body })
+})
+
+// Get all jobs
+app.get('/api/v1/jobs', (req, res) => {
+	res.status(200).json({ jobs })
+})
+
+// Create job
+app.post('/api/v1/jobs', (req, res) => {
+	const { company, position } = req.body
+	if (!company || !position) {
+		return res.status(400).json({ msg: 'please provide company and position' })
+	}
+
+	const id = nanoid(10)
+
+	const job = { id, company, position }
+
+	jobs.push(job)
+
+	res.status(200).json({ job })
 })
 
 const port = process.env.PORT || 5100
