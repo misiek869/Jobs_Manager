f15b4c625dfe2cd1f12f4e87f419c73aa3ebb785
Michal Gawik <gawlik.zator@o2.pl>
2024-11-16 06:29:14 +0100

    use morgan

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
 	res.send('hello world')
