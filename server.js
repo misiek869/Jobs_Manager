a066adc5e0b37568462a0ff451eeb16174aa12e2
Michal Gawik <gawlik.zator@o2.pl>
2024-11-16 06:18:39 +0100

    add middleware

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
 	res.send('hello world')
 })
 
+app.post('/', (req, res) => {
+	console.log(req)
+	res.json({ message: 'data received', data: req.body })
+})
+
 app.listen(5100, () => {
-	console.log('server is running')
+	console.log('server is running on port 5100')
 })
