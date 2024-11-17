8229d92f4e8ca60fbef6c0bad7637e565e7195e4
Michal Gawik <gawlik.zator@o2.pl>
2024-11-17 07:09:32 +0100

    refactor createJob to create job with mongoose schema

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,3 +1,5 @@
+import JobModel from '../models/JobModel.js'
+
 import { nanoid } from 'nanoid'
 
 let jobs = [
@@ -11,15 +13,11 @@ export const getAllJobs = async (req, res) => {
 
 export const createJob = async (req, res) => {
 	const { company, position } = req.body
-	if (!company || !position) {
-		return res.status(400).json({ msg: 'please provide company and position' })
-	}
 
-	const id = nanoid(10)
-
-	const job = { id, company, position }
-
-	jobs.push(job)
+	const job = await JobModel.create({
+		company,
+		position,
+	})
 
 	res.status(201).json({ job })
 }
