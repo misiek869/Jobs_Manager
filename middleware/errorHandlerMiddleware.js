6a244bb589cfc66527de0df85ab33083c25110cf
Michal Gawik <gawlik.zator@o2.pl>
2024-11-17 08:22:00 +0100

    create errorHandlerMiddleware and use

---
+++ middleware/errorHandlerMiddleware.js
@@ -0,0 +1,14 @@
+import { StatusCodes } from 'http-status-codes'
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+	console.log(err)
+
+	// statusCode from customErrors
+	const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
+
+	const msg = err.message || 'something went wrong'
+
+	res.status(statusCode).json({ msg: msg })
+}
+
+export default errorHandlerMiddleware
