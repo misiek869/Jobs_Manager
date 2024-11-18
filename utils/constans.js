5a2b99412043dd33f60ccbe8f0e424b910b20bdb
Michal Gawik <gawlik.zator@o2.pl>
2024-11-18 07:23:19 +0100

    create JOB_TYPE and use

--- utils/constans.js
+++ utils/constans.js
@@ -3,3 +3,16 @@ export const JOB_STATUS = {
 	INTERVIEW: 'interview',
 	DECLINED: 'declined',
 }
+
+export const JOB_TYPE = {
+	FULL_TIME: 'full-time',
+	PART_TIME: 'part-time',
+	INTERNSHIP: 'internship',
+}
+
+export const JOB_SORT_BY = {
+	NEWEST_FIRST: 'newest',
+	OLDEST_FIRST: 'oldest',
+	ASCENDING: 'a-z',
+	DESCENDING: 'z-a',
+}
