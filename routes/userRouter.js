b6a498b8e451465885543b5607f98e5eb4b1af30
Michal Gawik <gawlik.zator@o2.pl>
2024-11-18 14:14:11 +0100

    create routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,3 +1,10 @@
 import { Router } from 'express'
 
 const router = Router()
+
+import { createUser } from '../controllers/userController.js'
+
+router.post('/register', register)
+router.post('/login', login)
+
+export default router
