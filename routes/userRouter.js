29175435b1bc51742040c37cfdd8903cd10ce0b9
Michal Gawik <gawlik.zator@o2.pl>
2024-11-18 14:30:34 +0100

    create route to login

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,9 +2,9 @@ import { Router } from 'express'
 
 const router = Router()
 
-import { registerUSer } from '../controllers/userController.js'
+import { registerUSer, loginUser } from '../controllers/userController.js'
 
 router.post('/register', registerUSer)
-// router.post('/login', login)
+router.post('/login', loginUser)
 
 export default router
