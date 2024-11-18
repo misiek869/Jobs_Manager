1c71b0cdd7b045f86ba500168e855454fe24057c
Michal Gawik <gawlik.zator@o2.pl>
2024-11-18 14:42:44 +0100

    import an use validateRegisterUser

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
+import { validateRegisterUser } from '../middleware/validationMiddleware.js'
 
 const router = Router()
 
 import { registerUSer, loginUser } from '../controllers/userController.js'
 
-router.post('/register', registerUSer)
+router.post('/register', validateRegisterUser, registerUSer)
 router.post('/login', loginUser)
 
 export default router
