5a00639d92421e0782b83a7de96e618bca07bea1
Michal Gawik <gawlik.zator@o2.pl>
2024-11-18 14:12:00 +0100

    create createUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,17 @@
 import UserModel from '../models/UserModel.js'
 import { StatusCodes } from 'http-status-codes'
+
+export const createUser = async (req, res) => {
+	const { name, email, password, lastName, location, role } = req.body
+
+	const user = await UserModel.create({
+		name,
+		email,
+		password,
+		lastName,
+		location,
+		role,
+	})
+
+	res.status(StatusCodes.CREATED).json({ user })
+}
