911b604f96dcd382b737c83c5997e8118ca6b19a
Michal Gawik <gawlik.zator@o2.pl>
2024-11-19 05:35:29 +0100

    set first user as admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,12 @@ import UserModel from '../models/UserModel.js'
 import { StatusCodes } from 'http-status-codes'
 
 export const registerUSer = async (req, res) => {
+	const isFirstUser = (await UserModel.countDocuments()) === 0
+
+	req.body.role = isFirstUser ? 'admin' : 'user'
+
 	const user = await UserModel.create(req.body)
+
 	res.status(StatusCodes.CREATED).json({ user })
 }
 
