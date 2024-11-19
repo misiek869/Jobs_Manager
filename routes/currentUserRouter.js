8c991a7d6e0cd770679b175ac8945cf8f0c490fb
Michal Gawik <gawlik.zator@o2.pl>
2024-11-19 12:53:36 +0100

    add validateUpdateUser to update user path

--- routes/currentUserRouter.js
+++ routes/currentUserRouter.js
@@ -7,9 +7,10 @@ import {
 	getAppStats,
 	updateUser,
 } from '../controllers/currentUserController.js'
+import { validateUpdateUser } from '../middleware/validationMiddleware.js'
 
 router.get('/admin/stats', getAppStats)
 router.get('/current-user', getCurrentUser)
-router.patch('/update-user', updateUser)
+router.patch('/update-user', validateUpdateUser, updateUser)
 
 export default router
