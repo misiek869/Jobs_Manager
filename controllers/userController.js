eabc0a842b9ad0b9a1a73e2ba06eea9411e58c2e
Michal Gawik <gawlik.zator@o2.pl>
2024-11-19 06:06:13 +0100

    add search for user in loginUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import UserModel from '../models/UserModel.js'
 import { StatusCodes } from 'http-status-codes'
 import { hashedPassword } from '../utils/passwordUtils.js'
+import { Unauthenticated } from '../errors/customErrors.js'
 
 export const registerUSer = async (req, res) => {
 	const isFirstUser = (await UserModel.countDocuments()) === 0
@@ -15,5 +16,13 @@ export const registerUSer = async (req, res) => {
 }
 
 export const loginUser = async (req, res) => {
+	const { email } = req.body.email
+
+	const user = await UserModel.findOne({ email: email })
+
+	if (!user) {
+		throw new Unauthenticated('invalid credentials')
+	}
+
 	res.send('logged in')
 }
