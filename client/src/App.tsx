6c57889c4145424b2cb25f2bb9a05d75acd401b8
Michal Gawik <gawlik.zator@o2.pl>
2024-11-20 13:11:11 +0100

    add dashboardLoader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,6 +17,7 @@ import {
 
 import { action as registerAction } from './pages/Register'
 import { action as loginAction } from './pages/Login'
+import { loader as dashboardLoader } from './pages/DashboardLayout'
 
 export const checkDefaultTheme = (): boolean => {
 	const isDarkTheme = localStorage.getItem('theme') === 'true'
@@ -44,6 +45,7 @@ const router = createBrowserRouter([
 			{
 				path: 'dashboard',
 				element: <DashboardLayout />,
+				loader: dashboardLoader,
 				children: [
 					{
 						index: true,
