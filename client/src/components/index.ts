a7ac2dd1dad73562658ec59229d3ce0adba5cd3b
Michal Gawik <gawlik.zator@o2.pl>
2024-11-21 06:41:31 +0100

    add FormSelect to exports

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Logo } from './Logo'
 export { default as FormRow } from './FormRow'
+export { default as FormSelect } from './FormSelect'
 export { default as SidebarBig } from './SidebarBig'
 export { default as SidebarSmall } from './SidebarSmall'
 export { default as Navbar } from './Navbar'
