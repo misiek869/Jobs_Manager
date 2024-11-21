9a79c8ad479c0983acad7923118bafa4fa79cf35
Michal Gawik <gawlik.zator@o2.pl>
2024-11-21 08:33:52 +0100

    add action to add job page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,6 +17,7 @@ import {
 
 import { action as registerAction } from './pages/Register'
 import { action as loginAction } from './pages/Login'
+import { action as addJobAction } from './pages/AddJob'
 import { loader as dashboardLoader } from './pages/DashboardLayout'
 
 export const checkDefaultTheme = (): boolean => {
@@ -50,6 +51,7 @@ const router = createBrowserRouter([
 					{
 						index: true,
 						element: <AddJob />,
+						action: addJobAction,
 					},
 					{ path: 'stats', element: <Stats /> },
 					{ path: 'all-jobs', element: <AllJobs /> },
