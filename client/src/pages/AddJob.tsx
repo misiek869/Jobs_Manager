b4d95e5853ceab667bba8284b89442ce336ac200
Michal Gawik <gawlik.zator@o2.pl>
2024-11-21 05:50:18 +0100

    create form

--- client/src/pages/AddJob.tsx
+++ client/src/pages/AddJob.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { Form, useNavigation, useOutletContext } from 'react-router-dom'
+import { FormRow } from '../components'
 
 const AddJob = () => {
-	return <div>AddJob</div>
+	const { user } = useOutletContext()
+	const navigation = useNavigation()
+
+	const isSubmitting = navigation.state === 'submitting'
+
+	return (
+		<section className='w-full rounded-sm bg-red-300 pt-12 px-8 pb-16'>
+			<Form method='post' className='m-0 max-w-full w-full'>
+				<h4 className='capitalize mb-8'>add job</h4>
+				<div className='grid gap-y-4'>
+					<FormRow type='text' name='position' />
+				</div>
+			</Form>
+		</section>
+	)
 }
 
 export default AddJob
