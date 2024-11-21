4ac049d53a41a59ee3e19377ecffa407383cfa3b
Michal Gawik <gawlik.zator@o2.pl>
2024-11-21 08:29:48 +0100

    add styles

--- client/src/components/FormSelect.tsx
+++ client/src/components/FormSelect.tsx
@@ -15,12 +15,17 @@ const FormSelect = ({
 }: FormSelectProps) => {
 	return (
 		<div className='mb-0'>
-			{/* form label */}
-			<label className='' htmlFor={name}>
+			<label
+				className='block text-lg mb-3 capitalize tracking-wider leading-6 '
+				htmlFor={name}>
 				{labelText || name}
 			</label>
-			{/* form select */}
-			<select name={name} id={name} className='' defaultValue={defaultValue}>
+
+			<select
+				name={name}
+				id={name}
+				className='w-full py-[0.375rem] px-3 outline-none rounded-sm border'
+				defaultValue={defaultValue}>
 				{list.map(item => {
 					return (
 						<option key={item} value={item}>
