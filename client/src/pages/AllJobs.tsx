f5a8fedc0b9495fe5c54bd35440819d4f2e17bef
Michal Gawik <gawlik.zator@o2.pl>
2024-11-21 10:40:00 +0100

    create basic loader to fetch data

--- client/src/pages/AllJobs.tsx
+++ client/src/pages/AllJobs.tsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import customFetch from '../utils/customFetch'
+
+export const loader = async () => {
+	try {
+		const data = await customFetch.get('/jobs')
+		console.log(data)
+		return data
+	} catch (error) {}
+}
 
 const AllJobs = () => {
 	return <div>AllJobs</div>
