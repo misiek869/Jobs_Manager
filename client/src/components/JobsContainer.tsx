afa4ac0a4059ba478d04868183f81fbe6b7bf040
Michal Gawik <gawlik.zator@o2.pl>
2024-11-22 06:17:16 +0100

    style container

--- client/src/components/JobsContainer.tsx
+++ client/src/components/JobsContainer.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import Job from './Job'
+import { useAllJobsContext } from '../pages/AllJobs'
 
 const JobsContainer = () => {
-	return <div>jobs container</div>
+	const { data } = useAllJobsContext()
+	const { jobs } = data
+
+	if (jobs.length === 0) {
+		return (
+			<section className='mt-16'>
+				<h2 className=''>No jobs to display right now...</h2>
+			</section>
+		)
+	}
+
+	return (
+		<section className='mt-16'>
+			<div className='grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:gap-8'>
+				{jobs.map(job => {
+					return <Job key={job._id} {...job} />
+				})}
+			</div>
+		</section>
+	)
 }
 
 export default JobsContainer
