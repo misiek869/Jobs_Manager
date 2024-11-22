cb9181888695cf03e0cbe566e6c672757f8d15c8
Michal Gawik <gawlik.zator@o2.pl>
2024-11-22 06:36:10 +0100

    create structure of  Job

--- client/src/components/Job.tsx
+++ client/src/components/Job.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa'
+import day from 'dayjs'
+import JobInfo from './JobInfo'
+import { Form, Link } from 'react-router-dom'
 
-const Job = () => {
-	return <div>job</div>
+const Job = ({
+	_id,
+	position,
+	company,
+	jobLocation,
+	jobType,
+	createdAt,
+	jobStatus,
+}) => {
+	const date = day(createdAt).format('MMM Do, YYYY')
+
+	return (
+		<>
+			<header>
+				{/* main icon */}
+				<div className=''>{company.charAt(0)}</div>
+				<div className=''>
+					<h5>{position}</h5>
+					<p>{company}</p>
+				</div>
+			</header>
+			{/* content */}
+			<div className=''>
+				<div className=''>
+					<JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+					<JobInfo icon={<FaCalendarAlt />} text={date} />
+					<JobInfo icon={<FaBriefcase />} text={jobType} />
+					{/* dynamic */}
+					<div className=''>{jobStatus}</div>
+				</div>
+				<footer className=''>
+					<Link className='' to={'/'}>
+						Edit
+					</Link>
+					<Form>
+						<button className='' type='submit'>
+							Delete
+						</button>
+					</Form>
+				</footer>
+			</div>
+		</>
+	)
 }
 
 export default Job
