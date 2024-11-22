ab3c1bccdbfd6ef653029b654bdad1d99523a680
Michal Gawik <gawlik.zator@o2.pl>
2024-11-22 06:50:26 +0100

    style

--- client/src/components/Job.tsx
+++ client/src/components/Job.tsx
@@ -15,18 +15,19 @@ const Job = ({
 	const date = day(createdAt).format('MMM Do, YYYY')
 
 	return (
-		<>
-			<header>
-				{/* main icon */}
-				<div className=''>{company.charAt(0)}</div>
-				<div className=''>
-					<h5>{position}</h5>
-					<p>{company}</p>
+		<article className=' bg-orange-200 rounded-sm grid grid-rows-[1fr,auto] shadow-md'>
+			<header className='py-4 px-6 border-b grid grid-cols-[auto,1fr] items-center'>
+				<div className='w-[60px] h-[60px] grid place-items-center bg-orange-700 rounded-sm text-2xl font-semibold uppercase text-white mr-8'>
+					{company.charAt(0)}
+				</div>
+				<div>
+					<h5 className='mb-2'>{position}</h5>
+					<p className='m-0 capitalize tracking-wider '>{company}</p>
 				</div>
 			</header>
-			{/* content */}
-			<div className=''>
-				<div className=''>
+
+			<div className='py-4 px-6'>
+				<div className=' grid mt-4 mb-6 grid-cols-1 gap-y-6 items-center sm:grid-cols-2'>
 					<JobInfo icon={<FaLocationArrow />} text={jobLocation} />
 					<JobInfo icon={<FaCalendarAlt />} text={date} />
 					<JobInfo icon={<FaBriefcase />} text={jobType} />
@@ -44,7 +45,7 @@ const Job = ({
 					</Form>
 				</footer>
 			</div>
-		</>
+		</article>
 	)
 }
 
