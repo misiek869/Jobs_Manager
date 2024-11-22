a638c7095660fce8ea177e851ff5bc2acff76d78
Michal Gawik <gawlik.zator@o2.pl>
2024-11-22 11:35:41 +0100

    create loader

--- client/src/pages/EditJob.tsx
+++ client/src/pages/EditJob.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import customFetch from '../utils/customFetch'
+
+export const loader = async () => {
+	try {
+		const data = await customFetch.get('/dashboard/edit-job/:id')
+		console.log(data)
+	} catch (error) {}
+}
 
 const EditJob = () => {
 	return <div>EditJob</div>
