1fb0a93f8451015c2e67f7d72f677c6bd51907e7
Michal Gawik <gawlik.zator@o2.pl>
2024-11-22 11:10:37 +0100

    change styling, bg color

--- client/src/components/Job.tsx
+++ client/src/components/Job.tsx
@@ -16,14 +16,16 @@ const Job = ({
 	const date = day(createdAt).format('MMM Do, YYYY')
 
 	return (
-		<article className=' bg-orange-200 rounded-sm grid grid-rows-[1fr,auto] shadow-md'>
+		<article className=' bg-orange-700 rounded-sm grid grid-rows-[1fr,auto] shadow-md'>
 			<header className='py-4 px-6 border-b grid grid-cols-[auto,1fr] items-center'>
-				<div className='w-[60px] h-[60px] grid place-items-center bg-orange-700 rounded-sm text-2xl font-semibold uppercase text-white mr-8'>
+				<div className='w-[60px] h-[60px] grid place-items-center bg-white rounded-sm text-3xl font-semibold uppercase text-stone-900 mr-8'>
 					{company.charAt(0)}
 				</div>
 				<div>
-					<h5 className='mb-2'>{position}</h5>
-					<p className='m-0 capitalize tracking-wider '>{company}</p>
+					<h5 className='mb-2 text-stone-900 font-semibold'>{position}</h5>
+					<p className='m-0 capitalize tracking-wider font-medium text-stone-900'>
+						{company}
+					</p>
 				</div>
 			</header>
 
