1aae84861c95c4930b4009df352d54ebbc4a7c81
Michal Gawik <gawlik.zator@o2.pl>
2024-11-22 06:40:00 +0100

    create styles and body

--- client/src/components/JobInfo.tsx
+++ client/src/components/JobInfo.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-const JobInfo = () => {
-	return <div></div>
+const JobInfo = ({ icon, text }) => {
+	return (
+		<div className='flex items-center'>
+			<span className='text-[1rem] mr-4 flex items-center text-orange-700'>
+				{icon}
+			</span>
+			<span className='capitalize tracking-wider'>{text}</span>
+		</div>
+	)
 }
 
 export default JobInfo
