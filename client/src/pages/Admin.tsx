2d9710cb6748fe42b588fc4376cf06ba27779b74
Michal Gawik <gawlik.zator@o2.pl>
2024-11-23 05:38:34 +0100

    setup loader

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,4 +1,17 @@
-import React from 'react'
+import customFetch from '../utils/customFetch'
+import { CustomActionError } from '../utils/type'
+
+export const loader = async () => {
+	try {
+		const response = await customFetch('/users/admin/stats')
+		console.log(response.data)
+		return response
+	} catch (error) {
+		const customError = error as CustomActionError
+		// toast.error(customError.response?.data?.msg || 'An error occurred')
+		return error
+	}
+}
 
 const Admin = () => {
 	return <div>Admin</div>
