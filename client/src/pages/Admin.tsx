28ec55df0dcddd98ac253bbb373926c79d6abac3
Michal Gawik <gawlik.zator@o2.pl>
2024-11-23 05:44:27 +0100

    create loader to get data for admin page

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,19 +1,22 @@
+import { redirect, useLoaderData } from 'react-router-dom'
 import customFetch from '../utils/customFetch'
 import { CustomActionError } from '../utils/type'
+import { toast } from 'react-toastify'
 
 export const loader = async () => {
 	try {
 		const response = await customFetch('/users/admin/stats')
-		console.log(response.data)
 		return response
 	} catch (error) {
 		const customError = error as CustomActionError
-		// toast.error(customError.response?.data?.msg || 'An error occurred')
-		return error
+		toast.error(customError.response?.data?.msg || 'An error occurred')
+		return redirect('/dashboard')
 	}
 }
 
 const Admin = () => {
+	const { users, jobs } = useLoaderData()
+
 	return <div>Admin</div>
 }
 
