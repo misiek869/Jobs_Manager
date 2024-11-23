afe9976a16cea91be5a273cc74a0bf644a5e7dee
Michal Gawik <gawlik.zator@o2.pl>
2024-11-23 06:02:18 +0100

    create compinent

--- client/src/components/StatItem.tsx
+++ client/src/components/StatItem.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
-
-const StatItem = () => {
-	return <div></div>
+const StatItem = ({ count, title, icon, bcg }) => {
+	return (
+		<>
+			<header className=''>
+				<span className=''>{count}</span>
+				<span className=''>{icon}</span>
+			</header>
+			<h5 className=''>{title}</h5>
+		</>
+	)
 }
 
 export default StatItem
